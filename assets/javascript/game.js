d78467613a7e93b2598ee4e7c91fe24469e2b0c3
Briana Oursler <boursler@blackhole.localdomain>
2018-02-21 16:18:05 -0700

    fleshing out Character constructor and defining other necessary variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,28 @@
 //this is a constructor. Each individual character will be constructed from this blueprint
-function Character() {
+function Character(healthPower, attackPower, counterAttackPower) {
 	//characters have health, attackpower, counterattackpower properties
 	//characters have attack, counterattack, healthloss methods
-	this.healthPower;
-	this.attackPower;
-	this.counterAttackPower;
+	this.healthPower = healthPower;
+	this.attackPower = attackPower;
+	this.attackCount = 0;
+	this.counterAttackPower = counterAttackPower;
 	this.attack = function () {
 		//attack! returns attack value
+		this.attackCount++;
+		return this.attackPower * this.attackCount;
 	};
 	this.counterAttack = function () {
 		//attack back! returns counter attack value
+		return this.counterAttackPower;
 	};
 	this.healthLoss = function (value) {
 		//lose health. takes value passed in with attack amount, reduces overall health by that
 		//returns health
+		if (this.healthPower > 0) {
+			this.healthPower -= value;
+			return this.healthPower;
+		}
 	}
 }
-var playerCharacter = new Character
+var playerCharacter;
+var defenderList = [];
