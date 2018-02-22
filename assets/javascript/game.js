49ebb8edf971ff91f0b51a2111584f4127eede25
Briana Oursler <boursler@blackhole.localdomain>
2018-02-22 12:11:04 -0700

    introduce isRoundOver check, playGame function, initGame, document.onready and some logic testing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,60 @@
+
+//eventually, clicking your character will run the constructor on that character and set playerCharacter or enemyCharacter equal to their object
+var playerCharacter;
+var enemyCharacter;
+var char1 = "char1";
+var char2 = "char2";
+var char3 = "char3";
+var char4 = "char4";
+var defenderList = [char1, char2, char3, char4];
+
+function isRoundOver() {
+	if (enemyCharacter.healthPower <= 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+function isGameOver() {
+	if (playerCharacter.healthPower <= 0) {
+		return true;
+	}
+	else if (defenderList.length === 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+function playGame() {
+	enemyCharacter.healthLoss(playerCharacter.attack());
+	playerCharacter.healthLoss(enemyCharacter.attack());
+	if (isRoundOver()) {
+		if (!isGameOver()) {
+			//start new round
+		}
+		else {
+			//keep going
+		}
+	}
+	else {
+		//end game conditions
+	}
+}
+function initGame(char) {
+	//game starts with full defender list and beginning stats
+	var remove = defenderList.indexOf(char);
+	defenderList.splice(remove, 1);
+	console.log(defenderList);
+}
+initGame(char3);
 //this is a constructor. Each individual character will be constructed from this blueprint
-function Character(healthPower, attackPower, counterAttackPower) {
+function Character(name, healthPower, attackPower, counterAttackPower) {
 	//characters have health, attackpower, counterattackpower properties
 	//characters have attack, counterattack, healthloss methods
+	this.name = name;
 	this.healthPower = healthPower;
 	this.attackPower = attackPower;
 	this.attackCount = 0;
@@ -24,27 +77,10 @@ function Character(healthPower, attackPower, counterAttackPower) {
 		}
 	}
 }
-//eventually, clicking your character will run the constructor on that character and set playerCharacter or enemyCharacter equal to their object
-var playerCharacter;
-var enemyCharacter;
-var defenderList = [];
-///argggh does javascript have classes jsdlkjfsladkjfalskjdf
-function isRoundOver() {
-	if (enemyCharacter.healthPower <= 0) {
-		return true;
-	}
-	else {
-		return false;
-	}
+function GoodCharacter(healthPower, attackPower) {
+	Character(healthPower, attackPower, 0);
 }
-function isGameOver() {
-	if (playerCharacter.healthPower <= 0) {
-		return true;
-	}
-	else if (defenderList.length === 0) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
+
+$(document).ready(function () {
+	//here is where the first user input will be taken by a click event, kicking off the game and setting the machine into motion. No function will be called before this function
+});
