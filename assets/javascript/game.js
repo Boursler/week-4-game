9d327c8c03e711847c818aa3f4bec688a49c2cc8
Briana Oursler <boursler@blackhole.localdomain>
2018-02-22 12:25:58 -0700

    flesh out chooseCharacter and test

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 
 //eventually, clicking your character will run the constructor on that character and set playerCharacter or enemyCharacter equal to their object
-var playerCharacter;
+var playerCharacter = [];
 var enemyCharacter;
 var char1 = "char1";
 var char2 = "char2";
@@ -33,27 +33,37 @@ function playGame() {
 	playerCharacter.healthLoss(enemyCharacter.attack());
 	if (isRoundOver()) {
 		if (!isGameOver()) {
-			//start new round
+			chooseCharacter(char);
 		}
 		else {
 			//keep going
 		}
 	}
 	else {
-		//end game conditions
+		initGame();
 	}
 }
-function initGame(char) {
+function initGame() {
 	//game starts with full defender list and beginning stats
 
 }
-function chooseCharacter() {
+function chooseCharacter(char) {
+	//if a good guy has not been chosen yet, character is good guy
 	var remove = defenderList.indexOf(char);
-	playerCharacter = defenderList.splice(remove, 1);
-	console.log(defenderList);
-	console.log(playerCharacter);
+	if (playerCharacter.length === 0) {
+		playerCharacter = defenderList.splice(remove, 1);
+		playerCharacter = playerCharacter[0];
+		console.log(defenderList);
+		console.log(playerCharacter);
+	}
+	else {
+		//otherwise, bad guy
+		enemyCharacter = defenderList.splice(remove, 1);
+		enemyCharacter = enemyCharacter[0];
+		console.log(enemyCharacter);
+	}
 }
-initGame(char3);
+chooseCharacter(char3);
 //this is a constructor. Each individual character will be constructed from this blueprint
 function Character(name, healthPower, attackPower, counterAttackPower) {
 	//characters have health, attackpower, counterattackpower properties
@@ -81,9 +91,7 @@ function Character(name, healthPower, attackPower, counterAttackPower) {
 		}
 	}
 }
-function GoodCharacter(healthPower, attackPower) {
-	Character(healthPower, attackPower, 0);
-}
+
 
 $(document).ready(function () {
 	//here is where the first user input will be taken by a click event, kicking off the game and setting the machine into motion. No function will be called before this function
